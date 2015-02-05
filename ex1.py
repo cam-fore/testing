9468f2c91478cd04fff673dd7bbac55e3d808fee
cam-fore <cam.fore@gmail.com>
2015-02-05 19:54:09 +1100

    This is the first commit

---
+++ ex1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 18 23:02:43 2014
+
+@author: camilo
+"""
+
+
+print 'Hello world!'
+print 'Hello again'
+print 'I like typing this'
+print 'This is fun'
+print 'Yay! Printing'
+print "I'd much rather you 'not'"
+print 'I "said" do not touch this'
